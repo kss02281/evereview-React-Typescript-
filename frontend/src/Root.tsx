4732e3be0431a833ecd80de42f43d21439a2bfd8
[2기-1반]이선예 <cci3737@naver.com>
2021-11-21 22:36:57 +0900

    basicloginpage

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-<<<<<<< HEAD
-import { MainPage, LoginPage, NotFound } from './Pages';
-=======
 import { AboutPage, LoginPage, MainPage, NotFound } from './Pages';
->>>>>>> bc05af55516fdd865dea15f68e2e8e22514c0ad6
 import { Navbar } from './Components/common';
 import ROUTES from './constants/routes';
 
 const Root: React.FC = () => (
   <BrowserRouter>
     <Switch>
-<<<<<<< HEAD
-      <Route exact path={ROUTES.MAIN} component={MainPage} />
-      <Route path={ROUTES.LOGIN} component={LoginPage} />
-      <Route path={ROUTES.NotFound} component={NotFound} />
-      <Redirect path="*" to={ROUTES.NotFound} />
-=======
       <Route exact path={ROUTES.HOME} component={MainPage} />
       <Route path={ROUTES.ABOUT} component={AboutPage} />
       <Route path={ROUTES.LOGIN} component={LoginPage} />
       <Route path={ROUTES.NOTFOUND} component={NotFound} />
       <Redirect path="*" to="/notFound" />
->>>>>>> bc05af55516fdd865dea15f68e2e8e22514c0ad6
     </Switch>
   </BrowserRouter>
 );

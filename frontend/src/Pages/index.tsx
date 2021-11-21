7ecacae9485f58e3aa216dd642079fd4b4b6a421
[2기-1반]이선예 <cci3737@naver.com>
2021-11-21 15:35:31 +0900

    merge

--- frontend/src/Pages/index.tsx
+++ frontend/src/Pages/index.tsx
@@ -1,7 +1,4 @@
 export { default as MainPage } from './MainPage';
 export { default as LoginPage } from './LoginPage';
 export { default as NotFound } from './NotFound';
-export { default as LoginPage } from './LoginPage';
 export { default as AboutPage } from './AboutPage';
-
-

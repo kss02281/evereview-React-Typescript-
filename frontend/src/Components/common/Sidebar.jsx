846e26726506b197e510733243dd05c912eba9a9
dudejrtjdrp <dudejrtjdrp@naver.com>
2021-11-22 17:55:42 +0900

    sidebar icon 클릭 시 색상 변경 코드 압축

--- frontend/src/Components/common/Sidebar.jsx
+++ frontend/src/Components/common/Sidebar.jsx
@@ -10,22 +10,30 @@ const cx = classNames.bind(styles);
 
 function Sidebar () {
     const [dashBoardColor, setDashBoardColor] = useState(false);
-    const [contentsColor, setContentsColor] = useState(false);
-    const [profileColor, setProfileColor] = useState(false);
-    const [settingColor, setSettingColor] = useState(false);
-    const [notificationColor, setNotificationColor] = useState(false);
-    const [sideColor, setSideColor] = useState(false);
-    const handleClick = (e) => {
-        const targetClassName = e.target.className.baseVal
-        console.log(targetClassName)
-        if (sideColor === true){
-            setSideColor(false);
+    const [contentsColor, setContentsColor] = useState(true);
+    const [profileColor, setProfileColor] = useState(true);
+    const [settingColor, setSettingColor] = useState(true);
+    const [notificationColor, setNotificationColor] = useState(true);
+      function sideColorClick(e) {
+        console.log(e.currentTarget.id);
+        const colorId = e.currentTarget.id
+        setDashBoardColor(true);
+        setContentsColor(true);
+        setProfileColor(true);
+        setSettingColor(true);
+        setNotificationColor(true);
+        if (colorId === '1') {
+            setDashBoardColor(false);
+        } else if (colorId === '2') {
+            setContentsColor(false);
+        } else if (colorId === '3') {
+            setProfileColor(false);
+        } else if (colorId === '4') {
+            setSettingColor(false);
+        } else if (colorId === '5') {
+            setNotificationColor(false);
         }
-        else {
-            setSideColor(true);
-        }
-      };
-
+    }
     return (
         <>
             <div className={cx('sideBarContainer')}>
@@ -33,11 +41,11 @@ function Sidebar () {
                     <UilReact className={cx('sideLogoImage')}/>
                 </div>
                 <div className={cx('sideIcon')}>
-                    <UilDashboard className={cx('sideDashBoard')} style={sideColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={handleClick}/>
-                    <UilChart className={cx('sideContents')} style={sideColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={handleClick}/>
-                    <UilUserSquare className={cx('sideProfile')} style={sideColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={handleClick}/>
-                    <UilSetting className={cx('sideSetting')} style={sideColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={handleClick}/>
-                    <UilBell className={cx('sideNotification')} style={sideColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={handleClick}/>
+                    <UilDashboard className={cx('sideDashBoard')} id={1} style={dashBoardColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={sideColorClick}/>
+                    <UilChart className={cx('sideContents')} id={2} style={contentsColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={sideColorClick}/>
+                    <UilUserSquare className={cx('sideProfile')} id={3} style={profileColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={sideColorClick}/>
+                    <UilSetting className={cx('sideSetting')} id={4} style={settingColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={sideColorClick}/>
+                    <UilBell className={cx('sideNotification')} id={5} style={notificationColor ? {color:'#2f2f2f50'} : {color:'#6563FF'}} onClick={sideColorClick}/>
                 </div>
                 <div className={cx('sideProfile')}>
                     <UilUserCircle className={cx('sideUser')}/>
